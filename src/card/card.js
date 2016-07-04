5463b4bc1f8aee5399f4a17324213f4ef1dee02a
Oskar Kaminski <oskar.kaminski@smtsoftware.com>
2016-07-04 20:11:30 +0200

    Labels are created from the values in the card component

--- src/card/card.js
+++ src/card/card.js
@@ -1,5 +1,18 @@
 let cardCtrl = function(){
+    this.labels = {
+        11: 'J',
+        12: 'Q',
+        13: 'K',
+        14: 'A'
+    };
 
+    this.$onInit = () => {
+        this.label = this.getLabel();
+    }
+
+    this.getLabel = () => {
+        return this.labels[this.value] || this.value;
+    };
 };
 
 export let card = {
--- src/services/card/card.service.js
+++ src/services/card/card.service.js
@@ -1,7 +1,7 @@
 import * as _ from 'lodash'
 
 const cardValues = [
-    '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'
+    2,3,4,5,6,7,8,9,10,11,12,13,14
 ];
 
 const cardColors = [
