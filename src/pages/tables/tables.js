a2e53a1aea5564f6095b49cecb17f5bedf2ea056
Oskar Kaminski <oskar.kaminski@dporganizer.com>
2017-07-02 13:19:32 +0200

    Tables list

--- src/pages/tables/tables.js
+++ src/pages/tables/tables.js
@@ -1,16 +1,40 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import map from 'lodash/map';
 import {firebaseConnect, dataToJS} from 'react-redux-firebase'
+import {TablesListItem} from 'Molecule/TablesListItem/TablesListItem'
+import {Button} from 'Atom/Button/Button'
+import './tables.scss'
 
 @firebaseConnect(['tables'])
 @connect(({firebase}) => ({
     tables: dataToJS(firebase, 'tables')
 }))
 export class Tables extends React.Component {
+    addTable = () => {
+        this.props.firebase.push('/tables', {
+            id: 1,
+            name: 'table #1',
+            stake: '0.10/0.20',
+            seats: 4
+        })
+    }
+    onJoinTable = (id) => {
+        console.log(id);
+    }
     render() {
-        console.log(this.props.tables);
         return (
             <div className="tables">
+                <Button label="Add new table"
+                        onClick={this.addTable}/>
+                {
+                    this.props.tables &&
+                    map(this.props.tables, ((table, key)=>(
+                        <TablesListItem {...table}
+                                        onJoinTable={this.onJoinTable}
+                                        key={key}/>
+                    )))
+                }
             </div>
         )
     }
